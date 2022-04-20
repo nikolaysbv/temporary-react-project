d3b0f2e0faeb18ec9ced2e4b7da885ed7dafa557
nikolaysbv <89989774+nikolaysbv@users.noreply.github.com>
2022-04-20 15:46:58 +0300

    initial commit

---
+++ src/pages/About.js
@@ -0,0 +1,18 @@
+import React from "react"
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Ad deleniti
+        natus quas quidem, explicabo ipsam repellat aliquid fugit nesciunt
+        suscipit quia fuga quisquam excepturi, veritatis facere dolor doloribus
+        laudantium veniam beatae vitae! Dolorum dicta asperiores laudantium cum,
+        tenetur ratione natus.
+      </p>
+    </section>
+  )
+}
+
+export default About
